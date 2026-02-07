d061cfd744c5251bc252070c097694e9c56b7225
Dominik K. <dominik@rivo.gg>
2026-02-07 15:37:48 +0100

    fix: make welcome email personal

--- packages/email/src/emails/welcome.tsx
+++ packages/email/src/emails/welcome.tsx
@@ -16,32 +16,35 @@ export const WelcomeEmail = ({
 			<Head />
 			<Preview>Welcome to Notra - A quick note from the founder</Preview>
 			<Body>
-				<Text>Hey!</Text>
-
 				<Text>
-					I'm Dominik, the founder of Notra. I wanted to personally welcome you
-					and say thanks for signing up.
+					Hey I'm Dominik, the founder of Notra. I wanted to personally welcome
+					you and say thanks for signing up.
 				</Text>
 
 				<Text>
-					I built Notra because I was frustrated with how scattered content
-					creation had become. I hope it helps you as much as it's helped me.
+					I built Notra because I was shipping faster than ever but didn't have
+					enough time to come up with tweets, changelogs or linkedIn posts. I
+					hope you can stay long enough to see my full vision for Notra! :D
 				</Text>
 
 				<Text>
-					If you have any questions, feedback, or just want to chat - reply to
+					If you have any questions, feedback, or just want to chat reply to
 					this email. I read every single one.
+				</Text>
+
+				<Text>
+					Or schedule a talk with me at{" "}
+					<Link href="https://cal.com/dominikkoch">cal.com/dominikkoch</Link>!
+				</Text>
+
+				<Text>
+					btw you can get started at <Link href={appUrl}>app.usenotra.com</Link>
 				</Text>
 
 				<Text>
 					Cheers,
 					<br />
 					Dominik
-				</Text>
-
-				<Text>
-					P.S. You can get started at{" "}
-					<Link href={appUrl}>app.usenotra.com</Link>
 				</Text>
 
 				<Text style={{ fontSize: "12px", color: "#999", marginTop: "32px" }}>
