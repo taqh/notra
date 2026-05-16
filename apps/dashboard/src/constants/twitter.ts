37cb6ce3d542459f0fb161c765c4a01a5c93ea9b
Dominik K. <dominik@rivo.gg>
2026-05-16 09:03:01 +0200

    fix(twitter): default bare links to https

--- apps/dashboard/src/constants/twitter.ts
+++ apps/dashboard/src/constants/twitter.ts
@@ -2,11 +2,12 @@ export const TWITTER_CHAR_LIMIT = 280;
 export const MAX_IMPORT_POSTS = 20;
 export const TWITTER_BRAND_COLOR = "#000000";
 
-export const TWEET_MENTION_REGEX = /@(\w{1,15})/g;
+export const TWEET_MENTION_REGEX = /(?<![\w.])@(\w{1,15})/g;
 export const TWEET_HASHTAG_REGEX = /#(\w+)/g;
-export const TWEET_URL_REGEX = /(https?:\/\/[^\s<]+[^\s<.,:;"')\]!?])/g;
+export const TWEET_URL_REGEX =
+  /(?:https?:\/\/[^\s<]*[^\s<.,:;"')\]!?]|(?<![@\w.-])(?:www\.)?[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z]{2,})(?:\/[^\s<]*[^\s<.,:;"')\]!?])?)/g;
 export const TWEET_TOKEN_REGEX =
-  /(@\w{1,15}|#\w+|https?:\/\/[^\s<]+[^\s<.,:;"')\]!?])/g;
+  /((?<![\w.])@\w{1,15}|#\w+|(?:https?:\/\/[^\s<]*[^\s<.,:;"')\]!?]|(?<![@\w.-])(?:www\.)?[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z]{2,})(?:\/[^\s<]*[^\s<.,:;"')\]!?])?))/g;
 
 const TWITTER_PROFILE_IMAGE_SIZE_REGEX =
   /_(normal|bigger|mini|200x200|400x400)\./;
--- apps/dashboard/src/utils/format-tweet-content.tsx
+++ apps/dashboard/src/utils/format-tweet-content.tsx
@@ -5,10 +5,12 @@ import {
   TWEET_URL_REGEX,
 } from "@/constants/twitter";
 
+const URL_PROTOCOL_REGEX = /^https?:\/\//i;
+
 function getTweetTokenUrl(token: string): string | undefined {
   if (TWEET_URL_REGEX.test(token)) {
     TWEET_URL_REGEX.lastIndex = 0;
-    return token;
+    return URL_PROTOCOL_REGEX.test(token) ? token : `https://${token}`;
   }
   if (TWEET_MENTION_REGEX.test(token)) {
     TWEET_MENTION_REGEX.lastIndex = 0;
